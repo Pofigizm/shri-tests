e49b37a3f5a505287a030fa1b5cd08c27e00a662
Denis Iogansen <pofigizm@gmail.com>
2015-08-10 23:51:49 +0300

    add detail info

--- app/scripts/board.js
+++ app/scripts/board.js
@@ -4,10 +4,11 @@ var shriBoard;
 document.addEventListener('DOMContentLoaded', function(){
   console.time('render');
 
+  /* render starter html */
+
   var _body = document.querySelector('body'),
       container = document.querySelector('.board-wrapper'),
       fader = document.querySelector('.fader'),
-      detail = document.querySelector('.board-row-detail'),
       board = _node('table', 'board'),
       content = [],
       trArrival = 0,
@@ -33,6 +34,40 @@ document.addEventListener('DOMContentLoaded', function(){
   [0, 1].forEach(_addContent);
   container.appendChild(board);
 
+  /* end of render html */
+
+  console.timeEnd('render');
+  console.log(ShriData);
+
+  /* show/hide detail event listeners */
+
+  fader.addEventListener('click', function() {
+    _body.className = '';
+  });
+
+  board.addEventListener('click', function(event) {
+    var row = _closest(event.target, function(el) {
+        return [].indexOf.call(el.classList, 'board-row') !== -1; 
+      }),
+      detail = document.querySelector('.board-row-detail');
+
+    if (row) {
+      _body.className = 'show-detail';
+
+      fader.replaceChild(_node('div', 'board-row-detail', null, '<table>' +
+        shriBoard.reduce(function(res, el) {
+          return res + '<tr>' + 
+            '<td>' + el.name + '</td>' +
+            '<td>' + ShriData.data[row.dataset.index][el.field[0].value] + '</td>' +
+            '</tr>';
+        }, '') +
+        '</table>'), detail);
+    }
+  });
+
+  /* part load experiment */
+  /* it work if input data more then default show */
+
   board.addEventListener('scroll', function() {
     var center = document.body.clientHeight / 2,
         current = Number(content.filter(function(elem) {
@@ -47,37 +82,6 @@ document.addEventListener('DOMContentLoaded', function(){
     _removeContent(current + 2);
   });
 
-  fader.addEventListener('click', function() {
-    _body.className = '';
-  });
-
-  board.addEventListener('click', function(event) {
-    var row = _closest(event.target, function(el) {
-        return [].indexOf.call(el.classList, 'board-row') !== -1; 
-      }),
-      newDetail;
-
-    if (row) {
-      _body.className = 'show-detail';
-
-      newDetail = document.createElement('div');
-      newDetail.className = 'board-row-detail';
-      newDetail.innerHTML = '<table>' +
-        shriBoard.reduce(function(res, el) {
-          return res + _html({
-            tag: 'tr',
-            klass: el.klass,
-            value: 'name'
-          }, el);
-        }, '') + '</table>';
-        
-      fader.replaceChild(newDetail, detail);
-    }
-  });
-
-  console.timeEnd('render');
-  console.log(ShriData);
-
   function _getTypeClass(data) {
     var typeKlass = 'board-row';
 
@@ -90,16 +94,6 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     // -even and -odd is fallback :nth-child(2n of .class) selector
     return typeKlass;
-  }
-
-  function _getRow(tag, data, value) {
-    return function(res, el) {
-      return res + _html({
-        tag: tag,
-        klass: el.klass,
-        value: value || el.field
-      }, data || el);
-    };
   }
 
   function _addContent(index) {
@@ -130,6 +124,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
 });
 
+/* structure data */
+
 shriBoard = [
   {
     name: 'Тип',
@@ -147,12 +143,12 @@ shriBoard = [
     field: [
       {
         tag: 'span',
-        klass: 'board-number-code',
-        value: 'FlightNum'
-      }, {
-        tag: 'span',
         klass: 'board-number-full',
         value: 'FlightNumFull'
+      }, {
+        tag: 'span',
+        klass: 'board-number-code',
+        value: 'FlightNum'
       }
     ]
   }, {
@@ -161,12 +157,12 @@ shriBoard = [
     field: [
       {
         tag: 'span',
-        klass: 'board-company-code',
-        value: 'Carrier'
-      }, {
-        tag: 'span',
         klass: 'board-company-full',
         value: 'CarrierFull'
+      }, {
+        tag: 'span',
+        klass: 'board-company-code',
+        value: 'Carrier'
       }, {
         tag: 'img',
         klass: 'board-company-logo',
@@ -189,12 +185,12 @@ shriBoard = [
     field: [
       {
         tag: 'span',
-        klass: 'board-city-code',
-        value: 'CityCode'
-      }, {
-        tag: 'span',
         klass: 'board-city-full',
         value: 'CityFull'
+      }, {
+        tag: 'span',
+        klass: 'board-city-code',
+        value: 'CityCode'
       }
     ]
   }, {
@@ -230,13 +226,25 @@ shriBoard = [
   }
 ];
 
+/* util function */
+
+function _getRow(tag, data, value) {
+  return function(res, el) {
+    return res + _html({
+      tag: tag,
+      klass: el.klass,
+      value: value || el.field
+    }, data || el);
+  };
+}
+
 function _node(tag, klass, index, inner) {
   var node = document.createElement(tag || 'div');
 
   if (klass) {
     node.className = klass;
   }
-  if (index) {
+  if (_def(index)) {
     node.setAttribute('data-index', index);
   }
   node.innerHTML = inner || '';
@@ -256,25 +264,37 @@ function _html(tmpl, data) {
   href = data[tmpl.href];
 
   if ( Array.isArray(tmpl.value) ) {
-    value = tmpl.value.reduce(function(res, el) {
+    value = _wrap('div', klass, tmpl.value.reduce(function(res, el) {
       return res + _html(el, data);
-    }, '');
+    }, ''));
   } else {
     value = inner || String(data[tmpl.value] || 'text');
   }
 
   return '<' + tag +
-    (klass ? ' class="'      + klass + '"': '') +
-    (src   ? ' src="'        + src   + '"': '') +
-    (href  ? ' href="'       + href  + '"': '') +
-    (index ? ' data-index="' + index + '"' : '') +
+    (klass        ? ' class="'      + klass + '"': '') +
+    (src          ? ' src="'        + src   + '"': '') +
+    (href         ? ' href="'       + href  + '"': '') +
+    (_def(index)  ? ' data-index="' + index + '"' : '') +
     '>' +
     (noValueTag.indexOf(tag) > -1 ? '' : value) +
     '</' + tag + '>';
 }
 
+function _wrap(tag, klass, inner) {
+  return '<' + tag +
+    (klass ? ' class="' + klass + '"': '') +
+    '>' +
+    (inner ? inner : '') +
+    '</' + tag + '>';
+}
+
 function _closest(el, fx) {
   return el && (fx(el) ? el : _closest(el.parentNode, fx));
+}
+
+function _def(value) {
+  return typeof(value) !== 'undefined';
 }
 
 function _splitArr(arr, size) {

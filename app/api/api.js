71b1aeec2a3369e28dffb2566798fa3f13fdb75f
Denis Iogansen <pofigizm@gmail.com>
2015-08-16 23:39:41 +0300

    yandex codestyle 😎

--- app/api/api.js
+++ app/api/api.js
@@ -1,128 +1,128 @@
 
-function getData( url, callback ) {
-  var RESPONSES = {
-    '/countries': [
-      { name: 'Cameroon', continent: 'Africa' },
-      { name:'Fiji Islands', continent: 'Oceania' },
-      { name: 'Guatemala', continent: 'North America' },
-      { name: 'Japan', continent: 'Asia' },
-      { name: 'Yugoslavia', continent: 'Europe' },
-      { name: 'Tanzania', continent: 'Africa' }
-    ],
-    '/cities': [
-      { name: 'Bamenda', country: 'Cameroon' },
-      { name: 'Suva', country: 'Fiji Islands' },
-      { name: 'Quetzaltenango', country: 'Guatemala' },
-      { name: 'Osaka', country: 'Japan' },
-      { name: 'Subotica', country: 'Yugoslavia' },
-      { name: 'Zanzibar', country: 'Tanzania' }
-    ],
-    '/populations': [
-      { count: 138000, name: 'Bamenda' },
-      { count: 77366, name: 'Suva' },
-      { count: 90801, name: 'Quetzaltenango' },
-      { count: 2595674, name: 'Osaka' },
-      { count: 100386, name: 'Subotica' },
-      { count: 157634, name: 'Zanzibar' }
-    ]
-  };
+function getData(url, callback) {
+    var RESPONSES = {
+        '/countries': [
+          {name: 'Cameroon', continent: 'Africa'},
+          {name:'Fiji Islands', continent: 'Oceania'},
+          {name: 'Guatemala', continent: 'North America'},
+          {name: 'Japan', continent: 'Asia'},
+          {name: 'Yugoslavia', continent: 'Europe'},
+          {name: 'Tanzania', continent: 'Africa'}
+        ],
+        '/cities': [
+          {name: 'Bamenda', country: 'Cameroon'},
+          {name: 'Suva', country: 'Fiji Islands'},
+          {name: 'Quetzaltenango', country: 'Guatemala'},
+          {name: 'Osaka', country: 'Japan'},
+          {name: 'Subotica', country: 'Yugoslavia'},
+          {name: 'Zanzibar', country: 'Tanzania'}
+        ],
+        '/populations': [
+          {count: 138000, name: 'Bamenda'},
+          {count: 77366, name: 'Suva'},
+          {count: 90801, name: 'Quetzaltenango'},
+          {count: 2595674, name: 'Osaka'},
+          {count: 100386, name: 'Subotica'},
+          {count: 157634, name: 'Zanzibar'}
+        ]
+    };
 
-  setTimeout( function() {
-    var result = RESPONSES[ url ];
-    if ( !result ) {
-      return callback( 'Unknown url' );
-    }
+    setTimeout(function () {
+        var result = RESPONSES[ url ];
+        if (!result) {
+            return callback('Unknown url');
+        }
 
-    callback( null, result );
-  }, Math.round( Math.random * 1000 ) );
+        callback(null, result);
+    }, Math.round(Math.random() * 1000));
 }
 
-var requests = [ '/countries', '/cities', '/populations' ],
+var requests = ['/countries', '/cities', '/populations'],
   responses = {},
-  input = window.prompt( 'Input something (city, country or continent):', 'Africa' );
+  input = window.prompt('Input something (city, country or continent):', 'Africa');
 
-requests.forEach( function( request ) {
-  getData( request, function( error, result ) {
-    responses[request] = result;
+requests.forEach(function (request) {
+    getData(request, function (error, result) {
+        responses[request] = result;
 
-    // check all responses
-    if ( requests.every( _exist( responses ) ) ) {
-      var population = getPopulations( responses, input ).reduce( _add( 'count' ), 0 );
-      alert( 'Total population in ' + input + ' : ' + population );
+        // check all responses
+        if (requests.every(_exist(responses))) {
+            var population = getPopulations(responses, input).reduce(_add('count'), 0);
+            alert('Total population in ' + input + ' : ' + population);
+        }
+    });
+});
+
+function getPopulations(data, input) {
+    var populations,
+        countries,
+        cities;
+
+    // search input in cities
+    populations = data[ '/populations' ]
+      .filter(_compare(input, 'name'));
+
+    if (populations.length) {
+        return populations;
     }
-  } );
-} );
 
-function getPopulations( data, input ) {
-  var populations,
-      countries,
-      cities;
+    // search input in countries
+    cities = data[ '/cities' ]
+      .filter(_compare(input, 'country'))
+      .map(_get('name'));
 
-  // search input in cities
-  populations = data[ '/populations' ]
-    .filter( _compare( input, 'name' ) );
+    populations = data[ '/populations' ]
+      .filter(_compareSome(cities, 'name'));
 
-  if ( populations.length ) {
-    return populations;
-  }
+    if (populations.length) {
+        return populations;
+    }
 
-  // search input in countries
-  cities = data[ '/cities' ]
-    .filter( _compare( input, 'country' ) )
-    .map( _get( 'name' ) );
+    // search input in continents
+    countries = data[ '/countries' ]
+      .filter(_compare(input, 'continent'))
+      .map(_get('name'));
 
-  populations = data[ '/populations' ]
-    .filter( _compareSome( cities, 'name' ) );
+    cities = data[ '/cities' ]
+      .filter(_compareSome(countries, 'country'))
+      .map(_get('name'));
 
-  if ( populations.length ) {
-    return populations;
-  }
+    populations = data[ '/populations' ]
+      .filter(_compareSome(cities, 'name'));
 
-  // search input in continents
-  countries = data[ '/countries' ]
-    .filter( _compare( input, 'continent' ) )
-    .map( _get( 'name' ) );
+    if (populations.length) {
+        return populations;
+    }
 
-  cities = data[ '/cities' ]
-    .filter( _compareSome( countries, 'country' ) )
-    .map( _get( 'name' ) );
-
-  populations = data[ '/populations' ]
-    .filter( _compareSome( cities, 'name' ) );
-
-  if ( populations.length ) {
-    return populations;
-  }
-
-  return [];
+    return [];
 }
 
-function _exist( object ) {
-  return function( key ) {
-    return object.hasOwnProperty( key );
-  };
+function _exist(object) {
+    return function (key) {
+        return object.hasOwnProperty(key);
+    };
 }
 
-function _compare( value, key ) {
-  return function( element ) {
-    return value === ( key ? element[ key ] : element );
-  };
+function _compare(value, key) {
+    return function (element) {
+        return value === (key ? element[ key ] : element);
+    };
 }
 
-function _compareSome( array, key ) {
-  return function( element ) {
-    return array.some( _compare( key ? element[ key ] : element ) );
-  };
+function _compareSome(array, key) {
+    return function (element) {
+        return array.some(_compare(key ? element[ key ] : element));
+    };
 }
 
-function _add( key ) {
-  return function( result, element ) {
-    return result += element[ key ];
-  };
+function _add(key) {
+    return function (result, element) {
+        return result += element[ key ];
+    };
 }
 
-function _get( key ) {
-  return function( element ) {
-    return element[ key ];
-  };
+function _get(key) {
+    return function (element) {
+        return element[ key ];
+    };
 }
--- data.js
+++ data.js
@@ -4,7 +4,7 @@ var http = require('http'),
     httpFn, endFn, fsFn, exportCb;
 
 var airCraft = [
-  'Airbus A340', 
+  'Airbus A340',
   'Airbus A340',
   'Airbus A340',
   'Boeing 777',
@@ -19,58 +19,57 @@ var airCraft = [
 var statusList = ['Landed', 'Cancelled', 'Retimed', 'Confirmed'];
 
 option = {
-  host: 'www.flysfo.com',
-  path: '/flightprocessing/fullFlightData.txt'
+    host: 'www.flysfo.com',
+    path: '/flightprocessing/fullFlightData.txt'
 };
 
 file = 'app/board/data.json';
 
-httpFn = function(response) {
-  str = '';
-  console.timeEnd('http');
-  console.time('data');
-  response.on('data', function (chunk) {
-    str += chunk;
-  });
-  response.on('end', endFn);
+httpFn = function (response) {
+    str = '';
+    console.timeEnd('http');
+    console.time('data');
+    response.on('data', function (chunk) {
+        str += chunk;
+    });
+    response.on('end', endFn);
 };
 
-endFn = function() {
-  console.timeEnd('data');
-  console.time('parse');
-  data = JSON.parse(str).aaData;
-  fields = {};
-  data.forEach(function(elem){
-    Object.keys(elem).reduce(function(res, el){
-      res[el] = res[el] ? res[el] + 1 : 1;
-      return res;
-    }, fields);
-    elem['CarrierLogo'] = './logo.png';
-    elem['Aircraft'] = airCraft[Math.floor(Math.random() * 10)];
-    elem['Status'] = statusList[Math.floor(Math.random() * 4)];
-    elem['Comment'] = 'comment #' + Math.floor(Math.random() * 999);
-  });
-  fields = Object.keys(fields).sort(function(a, b) {
-    return fields[b] - fields[a];
-  });
-  console.timeEnd('parse');
-  console.time('write');
-  fs.writeFile(file, 'ShriData = ' + JSON.stringify({
-    head: fields,
-    data: data.slice(0, 100)
-  }, null, 2) + ';', fsFn);
+endFn = function () {
+    console.timeEnd('data');
+    console.time('parse');
+    data = JSON.parse(str).aaData;
+    fields = {};
+    data.forEach(function (elem) {
+        Object.keys(elem).reduce(function (res, el) {
+            res[el] = res[el] ? res[el] + 1 : 1;
+            return res;
+        }, fields);
+        elem['CarrierLogo'] = './logo.png';
+        elem['Aircraft'] = airCraft[Math.floor(Math.random() * 10)];
+        elem['Status'] = statusList[Math.floor(Math.random() * 4)];
+        elem['Comment'] = 'comment #' + Math.floor(Math.random() * 999);
+    });
+    fields = Object.keys(fields).sort(function (a, b) {
+        return fields[b] - fields[a];
+    });
+    console.timeEnd('parse');
+    console.time('write');
+    fs.writeFile(file, 'ShriData = ' + JSON.stringify({
+        head: fields,
+        data: data.slice(0, 100)
+    }, null, 2) + ';', fsFn);
 };
 
-fsFn = function(err) {
-  if (!err) {
-    console.timeEnd('write');
-    exportCb();
-  }
+fsFn = function (err) {
+    if (!err) {
+        console.timeEnd('write');
+        exportCb();
+    }
 };
 
 export function getShriData(callback) {
-  exportCb = callback;
-  console.time('http');
-  http.get(option, httpFn).end();
+    exportCb = callback;
+    console.time('http');
+    http.get(option, httpFn).end();
 }
-
